ebf55e0238355f8647cebaf98021143dd0263618
Bogdan Stamenic <bogdan.stamenic@tum.de>
2021-12-20 09:06:23 +0100

    Updated jankify script.

--- .bogdan/scripts/python/jankify.py
+++ .bogdan/scripts/python/jankify.py
@@ -1,21 +1,9 @@
 #!/usr/bin/env python3
 
-from random import seed, randint
 import argparse
 
-seed()
-def jostle(letter):
-    random_num = randint(0,1)
-    if random_num:
-        return letter.upper()
-    else:
-        return letter.lower()
-
 def jankify(s):
-    new_s = ""
-    for i in range(0, len(s)):
-        new_s += jostle(s[i])
-    return new_s
+    return ''.join([(s[x].upper(), s[x].lower())[x % 2] for x in range(len(s))])
 
 ## Parse cmd line input
 parser = argparse.ArgumentParser()
