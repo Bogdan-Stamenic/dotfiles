48c9693e6222b5b62386a07e308c738fe22d0710
Bogdan Stamenic <bogdan.stamenic@tum.de>
2022-01-06 09:07:31 +0100

    Update install script

--- .bogdan/scripts/bash/install_applications.sh
+++ .bogdan/scripts/bash/install_applications.sh
@@ -10,3 +10,6 @@ cmd='acpilight git screenkey-git texlive-most tmux'
 gui='atril firefox gparted keepassxc okular telegram-desktop thunar thunderbird'
 
 sudo pacman -Syu $editors $cmd $gui
+
+## Install paru for AUR applications
+cd ~/Downloads && git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si
