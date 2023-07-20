b143615d6a7ed4866e60cdbbe093bb3ca111d020
Bogdan Stamenic <bogdan.stamenic@tum.de>
2023-07-20 20:10:00 +0200

    Update before new system install.

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("~/org/journal.org" "/home/bogi/org/journal/2021-09.org" "/home/bogi/org/journal/2021-10.org" "/home/bogi/org/journal/2021-11.org" "/home/bogi/org/journal/2021-12.org" "/home/bogi/org/journal/2022-01.org" "/home/bogi/org/journal/2022-02.org" "/home/bogi/org/journal/2022-03.org" "/home/bogi/org/journal/2022-04.org" "/home/bogi/org/journal/2022-06.org" "/home/bogi/org/journal/2022-07.org" "/home/bogi/org/journal/2022-08.org" "/home/bogi/org/journal/2022-09.org" "/home/bogi/org/journal/2022-11.org" "/home/bogi/Documents/Journals/daily_journal.org")))
+   '("~/org/journal/2023-05.org" "/home/bogi/org/journal/2023-04.org" "/home/bogi/org/journal.org" "/home/bogi/org/journal/2021-09.org" "/home/bogi/org/journal/2021-10.org" "/home/bogi/org/journal/2021-11.org" "/home/bogi/org/journal/2021-12.org" "/home/bogi/org/journal/2022-01.org" "/home/bogi/org/journal/2022-02.org" "/home/bogi/org/journal/2022-03.org" "/home/bogi/org/journal/2022-04.org" "/home/bogi/org/journal/2022-06.org" "/home/bogi/org/journal/2022-07.org" "/home/bogi/org/journal/2022-08.org" "/home/bogi/org/journal/2022-09.org" "/home/bogi/org/journal/2022-11.org" "/home/bogi/Documents/Journals/daily_journal.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
